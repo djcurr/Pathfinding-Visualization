097893b43ee7870353f896f948a2d75460ed16ee
Derek Curry <djcurr@aol.com>
2022-05-21 00:46:19 -0400

    enable moving ndoes

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"math"
 	"strconv"
+	"strings"
 	"syscall/js"
 	"time"
 
@@ -29,26 +30,25 @@ type Nodes struct {
 var nodes Nodes
 var start bool
 var wallsEnabled = false
+var startMoving = false
+var endMoving = false
 
 func makeTable(rows int, columns int) string {
 
-	tableStr := "<table  draggable=\"false\">"
+	tableStr := `<table draggable="false" style="border-collapse: collapse">`
 
 	for i := 0; i < rows; i++ {
 
-		tableStr += "<tr draggable=\"false\">"
+		tableStr += `<tr draggable="false">`
 
 		for j := 0; j < columns; j++ {
 
-			elementLocation := strconv.Itoa(j) + "," + strconv.Itoa(i)
-			id := "id=\"" + elementLocation + "\""
-
 			if j == ((columns/8)-1) && i == (rows/2) {
-				makeStartNode(j, i, elementLocation, id, &tableStr)
+				makeStartNode(j, i, &tableStr)
 			} else if j == (columns-(columns/8)) && i == (rows/2) {
-				makeEndNode(j, i, elementLocation, id, &tableStr)
+				makeEndNode(j, i, &tableStr)
 			} else {
-				makeNormalNode(j, i, elementLocation, id, &tableStr)
+				makeNormalNode(j, i, &tableStr)
 			}
 
 		}
@@ -63,9 +63,11 @@ func makeTable(rows int, columns int) string {
 
 }
 
-func makeStartNode(j int, i int, elementLocation string, id string, tableStr *string) {
+func makeStartNode(j int, i int, tableStr *string) {
 
-	redBg := "style=\"background-color:red\""
+	elementLocation := strconv.Itoa(j) + "," + strconv.Itoa(i)
+	id := `id="` + elementLocation + `"`
+	redBg := `style="background-color:red"`
 
 	loc := Node{
 		location: [2]int{j, i},
@@ -75,12 +77,15 @@ func makeStartNode(j int, i int, elementLocation string, id string, tableStr *st
 	nodes.startNode = loc
 	nodes.nodeSlice = append(nodes.nodeSlice, loc)
 
-	*tableStr += "<td " + redBg + " draggable=\"true\"" + id + ">" + "</td>"
+	*tableStr += `<td ondragstart="enableMoveStart()" ` + redBg + ` draggable="true" ` + id + "></td>"
 }
 
-func makeEndNode(j int, i int, elementLocation string, id string, tableStr *string) {
+func makeEndNode(j int, i int, tableStr *string) {
 
-	blueBg := "style=\"background-color:blue\""
+	elementLocation := strconv.Itoa(j) + "," + strconv.Itoa(i)
+	id := `id="` + elementLocation + `"`
+
+	blueBg := `style="background-color:blue"`
 
 	loc := Node{
 		location: [2]int{j, i},
@@ -89,13 +94,16 @@ func makeEndNode(j int, i int, elementLocation string, id string, tableStr *stri
 	}
 	nodes.nodeSlice = append(nodes.nodeSlice, loc)
 
-	*tableStr += "<td " + blueBg + " draggable=\"true\"" + id + ">" + "</td>"
+	*tableStr += `<td ondragstart="enableMoveEnd()" ` + blueBg + ` draggable="true" ` + id + "></td>"
 
 	nodes.endNode = loc
 
 }
 
-func makeNormalNode(j int, i int, elementLocation string, id string, tableStr *string) {
+func makeNormalNode(j int, i int, tableStr *string) {
+
+	elementLocation := strconv.Itoa(j) + "," + strconv.Itoa(i)
+	id := `id="` + elementLocation + `"`
 
 	loc := Node{
 		location: [2]int{j, i},
@@ -104,7 +112,7 @@ func makeNormalNode(j int, i int, elementLocation string, id string, tableStr *s
 	}
 	nodes.nodeSlice = append(nodes.nodeSlice, loc)
 
-	*tableStr += "<td onmouseover=\"makeWall(" + elementLocation + ")\"" + id + ">" + "</td>"
+	*tableStr += `<td onpointerdown="toggleWalls()" ondrop="drop_handler(event)" ondragover="dragoverHandler(event)" ondragenter="dragoverHandler(event)" onmouseover="makeWall(` + elementLocation + `)"` + id + "></td>"
 
 }
 
@@ -165,7 +173,7 @@ func path(pathSlice []*Node) {
 		if node.location != nodes.startNode.location {
 			id := fmt.Sprintf("%v,%v", node.location[0], node.location[1])
 			el := dom.GetWindow().Document()
-			el.GetElementByID(id).SetAttribute("style", "background-color: yellow;")
+			el.GetElementByID(id).SetAttribute("style", "background-color: #f29668;")
 		}
 	}
 
@@ -195,7 +203,7 @@ func dijkstra() int {
 				if tempSrcNode.location != nodes.startNode.location {
 					id := fmt.Sprintf("%v,%v", tempSrcNode.location[0], tempSrcNode.location[1])
 					el := dom.GetWindow().Document()
-					el.GetElementByID(id).SetAttribute("style", "background-color: green;")
+					el.GetElementByID(id).SetAttribute("style", "background-color: #338070;")
 				}
 
 				if neighborNode.location == nodes.endNode.location {
@@ -210,7 +218,7 @@ func dijkstra() int {
 			if neighborNode.visited && neighborNode.location != nodes.startNode.location && !neighborNode.wall {
 				id := fmt.Sprintf("%v,%v", neighborNode.location[0], neighborNode.location[1])
 				el := dom.GetWindow().Document()
-				el.GetElementByID(id).SetAttribute("style", "background-color: green;")
+				el.GetElementByID(id).SetAttribute("style", "background-color: #338070;")
 			}
 
 		}
@@ -247,10 +255,105 @@ func toggleWalls(this js.Value, i []js.Value) interface{} {
 	return 0
 }
 
+func moveNode(this js.Value, i []js.Value) interface{} {
+	fmt.Println(startMoving, endMoving)
+	if startMoving {
+		locStr := i[0].String()
+		locStrArr := strings.Split(locStr, ",")
+		x, _ := strconv.Atoi(locStrArr[0])
+		y, _ := strconv.Atoi(locStrArr[1])
+		var newStartNode = ""
+
+		oldEl := dom.GetWindow().Document().GetElementByID(fmt.Sprintf("%v,%v", nodes.startNode.location[0], nodes.startNode.location[1]))
+		oldEl.SetAttribute("onpointerdown", "toggleWalls()")
+		oldEl.SetAttribute("ondrop", "drop_handler(event)")
+		oldEl.SetAttribute("ondragover", "dragoverHandler(event)")
+		oldEl.SetAttribute("ondragenter", "dragoverHandler(event)")
+		oldEl.SetAttribute("onmouseover", fmt.Sprintf("makeWall(%v,%v)", nodes.startNode.location[0], nodes.startNode.location[1]))
+		oldEl.SetAttribute("style", "")
+		oldEl.SetAttribute("ondragstart", "")
+		oldEl.SetAttribute("draggable", "false")
+
+		for idx := range nodes.nodeSlice {
+			if nodes.nodeSlice[idx].location == [2]int{nodes.startNode.location[0], nodes.startNode.location[1]} {
+				nodes.nodeSlice[idx].distance = math.MaxInt
+			}
+		}
+
+		makeStartNode(x, y, &newStartNode)
+
+		el := dom.GetWindow().Document().GetElementByID(fmt.Sprintf("%v,%v", x, y))
+		el.SetAttribute("onpointerdown", "")
+		el.SetAttribute("ondrop", "")
+		el.SetAttribute("ondragover", "")
+		el.SetAttribute("ondragenter", "")
+		el.SetAttribute("onmouseover", "")
+		el.SetAttribute("style", "background-color:red")
+		el.SetAttribute("ondragstart", "enableMoveStart()")
+		el.SetAttribute("draggable", "true")
+
+		startMoving = false
+
+	}
+	if endMoving {
+		locStr := i[0].String()
+		locStrArr := strings.Split(locStr, ",")
+		x, _ := strconv.Atoi(locStrArr[0])
+		y, _ := strconv.Atoi(locStrArr[1])
+		var newEndNode = ""
+
+		oldEl := dom.GetWindow().Document().GetElementByID(fmt.Sprintf("%v,%v", nodes.endNode.location[0], nodes.endNode.location[1]))
+		oldEl.SetAttribute("onpointerdown", "toggleWalls()")
+		oldEl.SetAttribute("ondrop", "drop_handler(event)")
+		oldEl.SetAttribute("ondragover", "dragoverHandler(event)")
+		oldEl.SetAttribute("ondragenter", "dragoverHandler(event)")
+		oldEl.SetAttribute("onmouseover", fmt.Sprintf("makeWall(%v,%v)", nodes.endNode.location[0], nodes.endNode.location[1]))
+		oldEl.SetAttribute("style", "")
+		oldEl.SetAttribute("ondragstart", "")
+		oldEl.SetAttribute("draggable", "false")
+
+		for idx := range nodes.nodeSlice {
+			if nodes.nodeSlice[idx].location == [2]int{nodes.endNode.location[0], nodes.endNode.location[1]} {
+				nodes.nodeSlice[idx].distance = math.MaxInt
+			}
+		}
+
+		makeEndNode(x, y, &newEndNode)
+
+		el := dom.GetWindow().Document().GetElementByID(fmt.Sprintf("%v,%v", x, y))
+		el.SetAttribute("onpointerdown", "")
+		el.SetAttribute("ondrop", "")
+		el.SetAttribute("ondragover", "")
+		el.SetAttribute("ondragenter", "")
+		el.SetAttribute("onmouseover", "")
+		el.SetAttribute("style", "background-color:blue")
+		el.SetAttribute("ondragstart", "enableMoveEnd()")
+		el.SetAttribute("draggable", "true")
+
+		endMoving = false
+
+	}
+
+	return 0
+}
+
+func enableMoveStart(this js.Value, i []js.Value) interface{} {
+	startMoving = true
+	return 0
+}
+
+func enableMoveEnd(this js.Value, i []js.Value) interface{} {
+	endMoving = true
+	return 0
+}
+
 func registerCallbacks() {
 	js.Global().Set("makeWall", js.FuncOf(makeWall))
 	js.Global().Set("run", js.FuncOf(run))
 	js.Global().Set("toggleWalls", js.FuncOf(toggleWalls))
+	js.Global().Set("moveNode", js.FuncOf(moveNode))
+	js.Global().Set("enableMoveStart", js.FuncOf(enableMoveStart))
+	js.Global().Set("enableMoveEnd", js.FuncOf(enableMoveEnd))
 }
 
 func main() {
